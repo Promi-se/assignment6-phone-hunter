ae06039fcc6109855d2c94e48b44501529436470
Promi-se <ismotjahanpromi99@gmail.com>
2023-04-11 01:34:51 +0600

    display phone details added

--- js/app.js
+++ js/app.js
@@ -64,3 +64,41 @@ const loadPhoneDetail = phoneId => {
     .then(res => res.json())
     .then(data => displayPhoneDetail(data.data))
 }
+
+// display Phone Detail function
+const displayPhoneDetail = (phone) => {
+    const phoneDetail = document.getElementById('phone-detail');
+    phoneDetail.textContent = '';
+    const div = document.createElement('div');
+    div.innerHTML = `
+      <div class="card border-success mb-10 mx-auto w-50">
+      <img src="${phone.image}" class="card-img-top w-50 mx-auto" alt="...">
+      <div class="card-body text-success">
+      <h4 class="card-title text-center">${phone.name}</h4>
+      <p> <b>Release Date:</b> <br>
+      ${phone.releaseDate ? phone.releaseDate: 'Release date is not available.' }</p>
+      <p><b>Main Features: </b> </br>
+      <b>Storage:</b> ${phone.mainFeatures.storage}. <br>
+      <b>Display Size:</b> ${phone.mainFeatures.displaySize}. <br>
+      <b>Chip Set:</b> ${phone.mainFeatures.chipSet}. <br>
+      <b>Memory:</b> ${phone.mainFeatures.memory}. <br>
+      <b>Storage:</b> ${phone.mainFeatures.storage}. <br>
+      <b>Sensors:</b> <br>
+      1. ${phone.mainFeatures.sensors[0]}. <br>
+      2. ${phone.mainFeatures.sensors[1]}. <br>
+      3. ${phone.mainFeatures.sensors[2]}. <br>
+      4. ${phone.mainFeatures.sensors[3]}. <br>
+      5. ${phone.mainFeatures.sensors[4]}. <br>
+      6. ${phone.mainFeatures.sensors[5]}.</p>
+      <b>Others:</b> <br>
+      <b>Bluetooth:</b> ${phone.others ? phone.others.Bluetooth: 'No Bluetooth Information'}. <br>
+      <b>GPS:</b> ${phone.others ? phone.others.GPS: 'No GPS Information'}. <br>
+      <b>NFC:</b> ${phone.others ? phone.others.NFC: 'No NFC Information'}. <br>
+      <b>Radio:</b> ${phone.others ? phone.others.Radio: 'No Radio Information'}. <br>
+      <b>USB:</b> ${phone.others ? phone.others.USB: 'No USB Information'}. <br>
+      <b>WLAN:</b> ${phone.others ? phone.others.WLAN: 'No WLAN Information'}. <br>
+      </div>
+      </div>
+      `;
+    phoneDetail.appendChild(div);
+}
